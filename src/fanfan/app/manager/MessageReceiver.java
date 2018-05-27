91f65ad71881b3843111a73bf0ae92b9b26f2026
3VNXP3VCA1PTYEN\Administrator <1154651663@qq.com>
2018-05-27 23:37:49 +0800

    登入

---
+++ src/fanfan/app/manager/MessageReceiver.java
@@ -0,0 +1,55 @@
+package fanfan.app.manager;
+
+import com.tencent.android.tpush.XGPushBaseReceiver;
+import com.tencent.android.tpush.XGPushClickedResult;
+import com.tencent.android.tpush.XGPushRegisterResult;
+import com.tencent.android.tpush.XGPushShowedResult;
+import com.tencent.android.tpush.XGPushTextMessage;
+
+import android.content.Context;
+
+public class MessageReceiver extends XGPushBaseReceiver {
+
+	@Override
+	public void onDeleteTagResult(Context arg0, int arg1, String arg2) {
+		// TODO Auto-generated method stub
+		 
+	}
+
+	@Override
+	public void onNotifactionClickedResult(Context arg0, XGPushClickedResult arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNotifactionShowedResult(Context arg0, XGPushShowedResult arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRegisterResult(Context arg0, int arg1, XGPushRegisterResult arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSetTagResult(Context arg0, int arg1, String arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTextMessage(Context arg0, XGPushTextMessage arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUnregisterResult(Context arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/fanfan/app/view/MainActivity.java
@@ -0,0 +1,65 @@
+package fanfan.app.view;
+ 
+import com.fanfan.app.R;
+import com.fanfan.app.R.id;
+import com.fanfan.app.R.layout;
+import com.fanfan.app.R.menu;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem; 
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		init(); 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	private void init() {
+		
+		 XGPushConfig.enableDebug(this,true);
+		 
+		 XGPushManager.registerPush(this,new XGIOperateCallback() {
+				 @Override
+				 public void onSuccess(Object data, int flag) {
+				 //token在设备卸载重装的时候有可能会变
+					 Log.d("TPush", "注册成功，设备token为：" + data);
+				 }
+				 @Override
+				 public void onFail(Object data, int errCode, String msg) {
+					 Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+				 }
+			 });
+		
+	}
+	
+}
